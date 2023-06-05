e6150161c3925253ac02d4393cbb20b547af922b
zk2840174 <zk2840174@gmail.com>
2023-06-05 13:58:44 +0900

    jsp config

--- src/main/java/org/zerock/bj1/controller/HelloController.java
+++ src/main/java/org/zerock/bj1/controller/HelloController.java
@@ -1,6 +1,7 @@
 package org.zerock.bj1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.log4j.Log4j2;
@@ -10,7 +11,9 @@ import lombok.extern.log4j.Log4j2;
 public class HelloController {
   
   @GetMapping("/hello")
-  public void hello(){
+  public void hello(Model model){
     log.info("Hello...............");
+
+    model.addAttribute("arr", new String[]{"AAA","BBB","CCC"});
   }
 }
